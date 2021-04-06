877e88bb6e094dd5642ccc024560e3e8095f75d5
今天也要开心呀 <2064233703@qq.com>
2021-04-06 09:35:53 +0800

    上传yyl

--- src/main/java/com/crm/controller/yyl/CustomerController.java
+++ src/main/java/com/crm/controller/yyl/CustomerController.java
@@ -1,7 +1,10 @@
 package com.crm.controller.yyl;
 
 import com.crm.dao.yyl.CustomerDao;
+import com.crm.entities.Customer;
+import com.crm.entities.Staff;
 import com.crm.service.yyl.CustomerService;
+import com.crm.service.yyl.PrincipallService;
 import com.crm.utils.RestContent;
 import com.crm.utils.ReturnContent;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class CustomerController {
     @Autowired
     CustomerService customerService;
+    @Autowired
+    PrincipallService pr;
     @Autowired
     private ReturnContent returnContent;
     //查询客户所有
@@ -38,5 +43,25 @@ public class CustomerController {
     @RequestMapping("/customer_advanced")
     public RestContent customer_advanced(String cuTime,String cuTimes,String cuState,String cuSource,String cuHigh,String cuRank){
         return returnContent.getContent(customerService.customer_advanced(cuTime, cuTimes, cuState, cuSource, cuHigh, cuRank),"数据获取成功","数据获取失败");
+    }
+    //新增客户
+    @RequestMapping("/cutomer_insert")
+    public void cutomer_insert(Customer ruleForm){
+        if(ruleForm.getCuId()==null){
+            Staff st=pr.staff_name(ruleForm.getStaffId());
+            customerService.cutomer_insert(ruleForm);
+        }else {
+            customerService.cutomer_update(ruleForm);
+        }
+    }
+    //客户回归到客户公海
+    @RequestMapping("/cutomer_flyback")
+    public void cutomer_flyback(Integer cuId){
+        customerService.cutomer_flyback(cuId);
+    }
+    //重新分配到客户公海
+    @RequestMapping("/cutomer_allocation")
+    public void cutomer_allocation(Integer staffId,Integer cuId){
+        customerService.cutomer_allocation(staffId, cuId);
     }
 }
--- src/main/java/com/crm/service/yyl/CustomerService.java
+++ src/main/java/com/crm/service/yyl/CustomerService.java
@@ -1,6 +1,7 @@
 package com.crm.service.yyl;
 
 import com.crm.dao.yyl.CustomerDao;
+import com.crm.entities.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,5 +33,22 @@ public class CustomerService {
     //高级查询客户
     public List<Map<String,Object>> customer_advanced(String cuTime,String cuTimes,String cuState,String cuSource,String cuHigh,String cuRank){
         return customerDao.customer_advanced(cuTime, cuTimes, cuState, cuSource, cuHigh, cuRank);
+    }
+    //客户新增
+    public void cutomer_insert(Customer customer){
+        customerDao.cutomer_insert(customer);
+    }
+    //客户修改
+    public void cutomer_update(Customer customer){
+        customerDao.cutomer_update(customer);
+    }
+    //客户归回客户公海
+    public void cutomer_flyback(Integer cuId){
+        customerDao.cutomer_flyback(cuId);
+    }
+
+    //重新分配负责人
+    public void cutomer_allocation(Integer staffId,Integer cuId){
+        customerDao.cutomer_allocation(staffId, cuId);
     }
 }
