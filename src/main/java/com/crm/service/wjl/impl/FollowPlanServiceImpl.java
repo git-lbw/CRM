295e378363a31f7d72d3f04434ced59905f5a83b
18273444526 <2466820706@qq.com>
2021-04-07 21:03:59 +0800

    04-07 wjl 21:03

--- src/main/java/com/crm/service/wjl/impl/FollowPlanServiceImpl.java
+++ src/main/java/com/crm/service/wjl/impl/FollowPlanServiceImpl.java
@@ -11,7 +11,7 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Service
-public class FollowPlanDaoImpl implements FollowPlanService {
+public class FollowPlanServiceImpl implements FollowPlanService {
 
     @Resource
     private FollowPlanDao followPlanService;
